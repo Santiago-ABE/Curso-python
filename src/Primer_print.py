3f66a4514cf3af9947b9e21e5c46bc49d11094ea
Santiago B <sanespitia7@gmail.com>
2025-04-08 19:17:15 -0500

    commit inicial

---
+++ src/Primer_print.py
@@ -0,0 +1,5 @@
+print("Hola Mundo!!!")
+
+print("1+1")
+
+print(1*1)
---
+++ src/clase3.py
@@ -0,0 +1,10 @@
+saludo1 = "Hola!!!"
+saludo2 = "¡¿Comó estas?!"
+nombre1 = "Santiago"
+nombre2 = "Andres"
+apellido = "Becerra"
+additinaltext = "*-*"
+print(saludo1)
+print(saludo2)
+print(nombre1, nombre2, apellido)
+print(additinaltext)
